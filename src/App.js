ed520b032aac3ff34151abc16ab8519a70fa0654
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-11-13 02:31:25 +0100

    feature: add language switcher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import { Center, ChakraProvider } from "@chakra-ui/react";
+import {
+  Center,
+  ChakraProvider,
+  Container,
+  Flex,
+  Heading,
+  Select,
+} from "@chakra-ui/react";
 import Login from "./components/login";
 
 function App() {
   return (
     <ChakraProvider>
-      <Center height="100vh">
+      <Flex
+        as="nav"
+        mx="5rem"
+        height="80px"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Heading as="h4" size="md">
+          Logo
+        </Heading>
+
+        <Select width="150px">
+          <option value="en">English</option>
+          <option value="fr">French</option>
+        </Select>
+      </Flex>
+
+      <Center height="calc(100vh - 80px)">
         <Login />
       </Center>
     </ChakraProvider>
